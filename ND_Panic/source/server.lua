6b18dacae688f73091b540762e069056bf7ac240
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-04-15 20:53:05 -0700

    Delete ND_Panic directory

--- ND_Panic/source/server.lua
+++
@@ -1,8 +0,0 @@
-NDCore = exports["ND_Core"]:GetCoreObject()
-
-if config["/panic"].enabled then
-    RegisterCommand("panic", function(source, args, rawCommand)
-        local player = source
-        TriggerClientEvent("ND_Panic:panic", -1, GetEntityCoords(GetPlayerPed(player)), table.concat(args, " "))
-    end, false)
-end
